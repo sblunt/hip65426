2e7cd4eaf590e6b3d5cc8a73930ca92176c7f41e
Sarah Blunt <sblunt@caltech.edu>
2023-09-07 17:02:17 -0700

    add in third grav point

--- fit_orbit.py
+++ fit_orbit.py
@@ -13,11 +13,14 @@ Fits to run:
 1. lit astrometry only (True, False, False, False, False)
 2. lit astrometry + first GRAVITY epoch (True, True, False, False, False)
 2.5. lit astrometry + second GRAVITY epoch (True, False, True, False, False)
-3. lit astrometry +2 GRAVITY epochs (True, True, True, False, False)
-4. GRAVITY astrometry only (False, True, True, False, False)
-5. all astrometry with e fixed to 0 (True, True, True, True, False)
-6. accepted fit (#3) with linearly decreasing prior on e (True, True, True, False, True)
+3. lit astrometry + first 2 GRAVITY (True, True, True, False, False)
+4. first 2 GRAVITY only (False, True, True, False, False)
+5. first 2 GRAVITY with e fixed to 0 (True, True, True, True, False)
+6. first 2 GRAVITY with linearly decreasing prior on e (True, True, True, False, True)
 
+7. all astrometry
+8. all astrometry with e fixed to 0
+9. all astrometry with linearly decreasing e prior
 """
 
 np.random.seed(10)
@@ -26,28 +29,32 @@ np.random.seed(10)
 Begin keywords <<
 """
 run_fit = False
+make_corner = True
 
 lit_astrom = True
 first_grav = True
 second_grav = True
+third_grav = True
 fix_ecc = False
-lin_ecc_prior = False
+lin_ecc_prior = True
 
-savedir = 'results/'
+savedir = "results/"
 
 if not os.path.exists(savedir):
     os.mkdir(savedir)
 
 if lit_astrom:
-    savedir += 'with_literature_astrom'
+    savedir += "with_literature_astrom"
 if first_grav:
-    savedir += 'with_first_vlti_point'
+    savedir += "with_first_vlti_point"
 if second_grav:
-    savedir += 'with_second_vlti_point'
+    savedir += "with_second_vlti_point"
+if third_grav:
+    savedir += "with_third_vlti_point"
 if fix_ecc:
-    savedir += '_fixed_ecc'
+    savedir += "_fixed_ecc"
 if lin_ecc_prior:
-    savedir += '_linear_ecc'
+    savedir += "_linear_ecc"
 """
 >> End keywords
 """
@@ -55,37 +62,46 @@ if lin_ecc_prior:
 if not os.path.exists(savedir):
     os.mkdir(savedir)
 
-input_file = 'HIP65426.csv'
-plx = 9.303088053872793 # [mas] (Gaia eDR3)
+input_file = "HIP65426.csv"
+plx = 9.303088053872793  # [mas] (Gaia eDR3)
 plx_err = 0.034559656
-m_st = 1.96 # [M_sol] (Bowler+ 2020)
+m_st = 1.96  # [M_sol] (Bowler+ 2020)
 mass_err = 0.04
 
 num_secondary_bodies = 1
 data_table = read_input.read_file(input_file)
-insts = data_table['instrument']
+insts = data_table["instrument"]
 
 if not lit_astrom:
     data_table = data_table[-3:]
 if not first_grav:
-    data_table = data_table[0:-1 & -1]
+    data_table = data_table[0 : -2 & -2 :]
 if not second_grav:
-    data_table = data_table[:-1]
+    data_table = data_table[0 : -1 & -1]
+if not third_grav:
+    data_table = data_table[0:-1]
 
 print(data_table)
 
 HIP654_system = system.System(
-    num_secondary_bodies, data_table, m_st, plx, fit_secondary_mass=False, mass_err=mass_err, 
-    plx_err=plx_err
+    num_secondary_bodies,
+    data_table,
+    m_st,
+    plx,
+    fit_secondary_mass=False,
+    mass_err=mass_err,
+    plx_err=plx_err,
 )
 
 # fix eccentricity to 0
 if fix_ecc:
-    HIP654_system.sys_priors[HIP654_system.param_idx['ecc1']] = 0
+    HIP654_system.sys_priors[HIP654_system.param_idx["ecc1"]] = 0
 
 # set a linearly decreasing prior on ecc
 if lin_ecc_prior:
-    HIP654_system.sys_priors[HIP654_system.param_idx['ecc1']] = priors.LinearPrior(-2.18, 2.01)
+    HIP654_system.sys_priors[HIP654_system.param_idx["ecc1"]] = priors.LinearPrior(
+        -2.18, 2.01
+    )
 
 # Check that orbitizie! initialized everything correctly.
 # (I wrote the code, therefore I do not trust the code.)
@@ -93,89 +109,135 @@ assert not HIP654_system.fit_secondary_mass
 assert not HIP654_system.track_planet_perturbs
 
 # run MCMC
-num_threads = 15
+num_threads = 50
 num_temps = 20
 num_walkers = 1000
-num_steps = 100_000_000 # n_walkers x n_steps_per_walker
+num_steps = 200_000_000  # n_walkers x n_steps_per_walker
 burn_steps = 100_000
 thin = 100
 
 if run_fit:
-
     HIP654_sampler = sampler.MCMC(
-        HIP654_system, num_threads=num_threads, num_temps=num_temps, 
-        num_walkers=num_walkers
+        HIP654_system,
+        num_threads=num_threads,
+        num_temps=num_temps,
+        num_walkers=num_walkers,
     )
     HIP654_sampler.run_sampler(num_steps, burn_steps=burn_steps, thin=thin)
 
     # save chains
-    HIP654_sampler.results.save_results(
-        '{}/chains.hdf5'.format(savedir)
-    )
+    HIP654_sampler.results.save_results("{}/chains.hdf5".format(savedir))
 
-# make corner plot
-HIP654_results = results.Results() # create blank results object for loading
-HIP654_results.load_results('{}/chains.hdf5'.format(savedir))
+HIP654_results = results.Results()  # create blank results object for loading
+HIP654_results.load_results("{}/chains.hdf5".format(savedir))
 
 # chop chains
 num_chop = 1000
-reshaped_post = HIP654_results.post.reshape((num_walkers,num_steps//num_walkers//thin,HIP654_results.post.shape[1]))
-HIP654_results.post = reshaped_post[:,-num_chop:,:].reshape((-1,HIP654_results.post.shape[1]))
-
-if fix_ecc:
-    param_list = ['sma1','inc1','aop1','pan1','tau1','mtot','plx']
-else:
-    param_list = None
-
-# median_values = np.median(HIP654_results.post, axis=0)
-# range_values = np.ones_like(median_values)*0.997 # Plot only 3-sigma range for each parameter
-fig = HIP654_results.plot_corner(
-    param_list=param_list, range=[
-        (40, 120), 1, 1, 1, 1, 1, 1, 1
-    ]
+reshaped_post = HIP654_results.post.reshape(
+    (num_walkers, num_steps // num_walkers // thin, HIP654_results.post.shape[1])
 )
-plt.savefig('{}/corner.png'.format(savedir), dpi=250)
+HIP654_results.post = reshaped_post[:, -num_chop:, :].reshape(
+    (-1, HIP654_results.post.shape[1])
+)
+
+# make corner plot
+if make_corner:
+    if fix_ecc:
+        param_list = ["sma1", "inc1", "aop1", "pan1", "tau1", "mtot", "plx"]
+    else:
+        param_list = None
+
+    # median_values = np.median(HIP654_results.post, axis=0)
+    # range_values = np.ones_like(median_values)*0.997 # Plot only 3-sigma range for each parameter
+    fig = HIP654_results.plot_corner(
+        param_list=param_list, range=[(40, 120), 1, 1, 1, 1, 1, 1, 1]
+    )
+    plt.savefig("{}/corner.png".format(savedir), dpi=250)
 
 # make orbit plot
-fig = HIP654_results.plot_orbits(num_epochs_to_plot=500, start_mjd=56000, plot_astrometry=False)
+fig = HIP654_results.plot_orbits(
+    num_epochs_to_plot=500, start_mjd=56000, plot_astrometry=False
+)
 radec_ax, sep_ax, pa_ax, cbar_ax = fig.axes
 
-sep, serr, pa, paerr = data_table['quant1'],  data_table['quant1_err'], data_table['quant2'], data_table['quant2_err']
-epoch = Time(data_table['epoch'], format='mjd').decimalyear
+sep, serr, pa, paerr = (
+    data_table["quant1"],
+    data_table["quant1_err"],
+    data_table["quant2"],
+    data_table["quant2_err"],
+)
+epoch = Time(data_table["epoch"], format="mjd").decimalyear
 
-sphere_mask = np.where(insts == 'SPHERE')[0]
-naco_mask = np.where(insts == 'NACO')[0]
-grav_mask = np.where(insts == 'GRAVITY')[0]
+sphere_mask = np.where(insts == "SPHERE")[0]
+naco_mask = np.where(insts == "NACO")[0]
+grav_mask = np.where(insts == "GRAVITY")[0]
 
 gravity_ra, gravity_dec = sep[grav_mask], pa[grav_mask]
 sphere_ra, sphere_dec = system.seppa2radec(sep[sphere_mask], pa[sphere_mask])
 naco_ra, naco_dec = system.seppa2radec(sep[naco_mask], pa[naco_mask])
 
-radec_ax.scatter(sphere_ra, sphere_dec, marker='o', color='hotpink', zorder=20, s=3)
-radec_ax.scatter(naco_ra, naco_dec, marker='o', color='hotpink', zorder=20, s=3)
-radec_ax.scatter(gravity_ra, gravity_dec, marker='o', color='hotpink', zorder=20, s=3)
+radec_ax.scatter(sphere_ra, sphere_dec, marker="o", color="hotpink", zorder=20, s=3)
+radec_ax.scatter(naco_ra, naco_dec, marker="o", color="hotpink", zorder=20, s=3)
+radec_ax.scatter(gravity_ra, gravity_dec, marker="o", color="hotpink", zorder=20, s=3)
 
 gravity_sep, gravity_pa = system.radec2seppa(sep[grav_mask], pa[grav_mask])
 
-gravity_raerr, gravity_decerr, gravity_corr = data_table['quant1_err'][grav_mask], data_table['quant2_err'][grav_mask], data_table['quant12_corr'][grav_mask], 
+gravity_raerr, gravity_decerr, gravity_corr = (
+    data_table["quant1_err"][grav_mask],
+    data_table["quant2_err"][grav_mask],
+    data_table["quant12_corr"][grav_mask],
+)
 
-sep_ax.errorbar(epoch[sphere_mask], sep[sphere_mask], serr[sphere_mask], marker='^', color='purple', markeredgecolor='purple', markerfacecolor='white',ls='', label='SPHERE')
-sep_ax.errorbar(epoch[naco_mask], sep[naco_mask], serr[naco_mask], marker='s', ls='', color='purple', label='NACO')
-sep_ax.scatter(epoch[grav_mask], gravity_sep, label='GRAVITY', zorder=20, color='hotpink')
+sep_ax.errorbar(
+    epoch[sphere_mask],
+    sep[sphere_mask],
+    serr[sphere_mask],
+    marker="^",
+    color="purple",
+    markeredgecolor="purple",
+    markerfacecolor="white",
+    ls="",
+    label="SPHERE",
+)
+sep_ax.errorbar(
+    epoch[naco_mask],
+    sep[naco_mask],
+    serr[naco_mask],
+    marker="s",
+    ls="",
+    color="purple",
+    label="NACO",
+)
+sep_ax.scatter(
+    epoch[grav_mask], gravity_sep, label="GRAVITY", zorder=20, color="hotpink"
+)
 sep_ax.legend()
 
-pa_ax.errorbar(epoch[sphere_mask], pa[sphere_mask], paerr[sphere_mask], marker='^', color='purple', markeredgecolor='purple', markerfacecolor='white',ls='')
-pa_ax.errorbar(epoch[naco_mask], pa[naco_mask], paerr[naco_mask], marker='s', ls='', color='purple')
-pa_ax.scatter(epoch[grav_mask], gravity_pa, zorder=20, color='hotpink')
+pa_ax.errorbar(
+    epoch[sphere_mask],
+    pa[sphere_mask],
+    paerr[sphere_mask],
+    marker="^",
+    color="purple",
+    markeredgecolor="purple",
+    markerfacecolor="white",
+    ls="",
+)
+pa_ax.errorbar(
+    epoch[naco_mask], pa[naco_mask], paerr[naco_mask], marker="s", ls="", color="purple"
+)
+pa_ax.scatter(epoch[grav_mask], gravity_pa, zorder=20, color="hotpink")
 
 l, b, w, h = cbar_ax.get_position().bounds
-cbar_ax.set_position([l - 0.05, b, w, h])
+# cbar_ax.set_position([l - 0.05, b, w, h])
+cbar_ax.set_position([l - 0.17, b, w, h])
 
 l, b, w, h = pa_ax.get_position().bounds
-pa_ax.set_position([l - 0.125, b, w, h])
 
 
-def confidence_ellipse(x, y, corr, x_unc, y_unc, ax, n_std=3.0, facecolor='hotpink', alpha=1):
+def confidence_ellipse(
+    x, y, corr, x_unc, y_unc, ax, n_std=3.0, facecolor="hotpink", alpha=1
+):
     """
     Create a plot of the covariance confidence ellipse of *x* and *y*.
     (shamelessly stolen from matplotlib docs)
@@ -185,8 +247,14 @@ def confidence_ellipse(x, y, corr, x_unc, y_unc, ax, n_std=3.0, facecolor='hotpi
     # two-dimensional dataset.
     ell_radius_x = np.sqrt(1 + corr)
     ell_radius_y = np.sqrt(1 - corr)
-    ellipse = patches.Ellipse((0,0), width=ell_radius_x * 2, height=ell_radius_y * 2,
-                      facecolor=facecolor, alpha=alpha, zorder=20)
+    ellipse = patches.Ellipse(
+        (0, 0),
+        width=ell_radius_x * 2,
+        height=ell_radius_y * 2,
+        facecolor=facecolor,
+        alpha=alpha,
+        zorder=20,
+    )
 
     # Calculating the standard deviation of x from
     # the squareroot of the variance and multiplying
@@ -198,39 +266,57 @@ def confidence_ellipse(x, y, corr, x_unc, y_unc, ax, n_std=3.0, facecolor='hotpi
     scale_y = y_unc * n_std
     mean_y = y
 
-    transf = transforms.Affine2D() \
-        .rotate_deg(45) \
-        .scale(scale_x, scale_y) \
+    transf = (
+        transforms.Affine2D()
+        .rotate_deg(45)
+        .scale(scale_x, scale_y)
         .translate(mean_x, mean_y)
+    )
 
     ellipse.set_transform(transf + ax.transData)
     return ax.add_patch(ellipse)
 
-fig.subplots_adjust(right=0.75)
-for i in np.arange(len(grav_mask)):
 
-    grav_ax = fig.add_axes([0.82, b + .42*i, 0.175, h])
-    for n_std in [1,2]:
+fig.subplots_adjust(right=0.6)
+radec_ax.set_position([0.05, b, w - 0.03, 0.77])
+pa_ax.set_position([l - 0.23, b, w - 0.06, h])
+sep_ax.set_position([l - 0.23, b + 0.42, w - 0.06, h])
+
+for i in np.arange(len(grav_mask)):
+    if i > 1:
+        grav_ax = fig.add_axes([0.82, b, 0.1, h])
+    else:
+        grav_ax = fig.add_axes([0.68, b + 0.42 * i, 0.1, h])
+    for n_std in [1, 2]:
         ellipse = confidence_ellipse(
-            gravity_ra[i], gravity_dec[i], gravity_corr[i], gravity_raerr[i], 
-            gravity_decerr[i], grav_ax, n_std=n_std, alpha=1 - n_std/4
+            gravity_ra[i],
+            gravity_dec[i],
+            gravity_corr[i],
+            gravity_raerr[i],
+            gravity_decerr[i],
+            grav_ax,
+            n_std=n_std,
+            alpha=1 - n_std / 4,
         )
 
-    grav_ax.set_xlim(gravity_ra[i] + 0.5, gravity_ra[i] - 0.5)
+    grav_ax.set_xlim(gravity_ra[i] + 0.3, gravity_ra[i] - 0.3)
     grav_ax.set_ylim(gravity_dec[i] - 0.5, gravity_dec[i] + 0.5)
-    grav_ax.set_aspect('equal')
+    grav_ax.set_aspect("equal")
 
-    if i == 0:
-        grav_ax.set_xlabel('$\Delta$RA [mas]')
-    grav_ax.set_ylabel('$\Delta$Dec [mas]')
+    if i == 0 or i == 2:
+        grav_ax.set_xlabel("$\Delta$RA [mas]")
+    if i < 2:
+        grav_ax.set_ylabel("$\Delta$Dec [mas]")
 
     for j in np.arange(len(sep_ax.lines) - 2):
         orbittracks_sep = sep_ax.lines[j].get_ydata()
         orbittracks_pa = pa_ax.lines[j].get_ydata()
         ra2plot, dec2plot = system.seppa2radec(orbittracks_sep, orbittracks_pa)
-        grav_ax.plot(ra2plot, dec2plot, color='lightgrey')
-        grav_ax.text(gravity_ra[i] + 0.45, gravity_dec[i] + 0.4, 'GRAVITY Epoch {}'.format(i + 1))
+        grav_ax.plot(ra2plot, dec2plot, color="lightgrey")
+        grav_ax.text(
+            gravity_ra[i] + 0.27, gravity_dec[i] + 0.4, "Epoch {}".format(i + 1)
+        )
 
-pa_ax.set_xlabel('Epoch [year]')
-radec_ax.set_aspect('equal')
-plt.savefig('{}/orbit.png'.format(savedir), dpi=250)
+pa_ax.set_xlabel("Epoch [year]")
+radec_ax.set_aspect("equal")
+plt.savefig("{}/orbit.png".format(savedir), dpi=250)
